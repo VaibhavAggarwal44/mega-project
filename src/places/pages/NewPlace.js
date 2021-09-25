4daa1f8c094ad1d9e8fddce44a7fdaf52e80758e
vaibhav aggarwal <vaibhav4nov@gmail.com>
2021-09-25 12:49:10 +0530

    form validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter Valid Title"
+      />
     </form>
   );
 };
